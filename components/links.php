1f64ddb7ce23d7fc30c42ee4d22a11921bad3a75
Anjuka Koralage <koralage.2014022@iit.ac.lk>
2018-12-04 21:37:22 -0800

    First Commit

---
+++ components/links.php
@@ -0,0 +1,4 @@
+<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+<link type="text/css" rel="stylesheet" href="css/jquery.mobile-1.4.5.min.css">
+<link type="text/css" rel="stylesheet" href="css/style.css">
+<script type="text/javascript" src="js/jquery.mobile-1.4.5.min.js"></script>
---
+++ index.php
@@ -0,0 +1,58 @@
+<?php include('components/links.php');?>
+
+<!DOCTYPE html>
+<html lang="en" class="font">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Title</title>
+</head>
+
+<body>
+<!--
+<div data-role="page" id="intro" style="background-color:#050517">
+
+    <div data-role="content" align="center">
+        <img class="splash-logo" src="images/logo.png"/>
+        <h1 class="footer">A Solution by Team 420 Coders</h1>
+    </div>
+</div>-->
+
+<div data-role="page" id="page2">
+    <div data-role="header">
+        <?php include('components/header.php');?>
+    </div>
+    <div data-role="content">
+        Content
+    </div>
+    <div data-role="footer">
+        <?php include('components/footer.php');?>
+    </div>
+</div>
+
+<!--<div data-role="page" id="page3">
+    <div data-role="header">
+        <h1>Page Three</h1>
+    </div>
+    <div data-role="content">
+        Content
+    </div>
+    <div data-role="footer">
+        <h4>Page Footer</h4>
+    </div>
+</div>
+
+<div data-role="page" id="page4">
+    <div data-role="header">
+        <h1>Page Four</h1>
+    </div>
+    <div data-role="content">
+        Content
+    </div>
+    <div data-role="footer">
+        <h4>Page Footer</h4>
+    </div>
+</div>-->
+
+</body>
+</html>
